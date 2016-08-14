0dc8e06ebd985db397496a2c1aa71bf71d47fcb0
Danii <Danii@Samsung-Note>
2016-08-14 14:34:11 -0300

    #4 EJB's
    (Pulei o commit 3, porém o passo foi feito. Dupliquei o commit 2)

---
+++ src/main/java/br/univel/EJBs/ProcessEntrega.java
@@ -0,0 +1,14 @@
+package br.univel.EJBs;
+
+import javax.ejb.Stateless;
+
+import br.univel.servlets.Entrega;
+
+@Stateless
+public class ProcessEntrega {
+
+    public void processarEntrega(Entrega entrega) {
+
+    }
+
+}
---
+++ src/main/java/br/univel/EJBs/ProcessVenda.java
@@ -0,0 +1,14 @@
+package br.univel.EJBs;
+
+import javax.ejb.Stateless;
+
+import br.univel.servlets.Venda;
+
+@Stateless
+public class ProcessVenda {
+
+    public void processarVenda(Venda venda) {
+
+    }
+
+}
